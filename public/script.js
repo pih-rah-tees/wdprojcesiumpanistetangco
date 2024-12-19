56733f7a3a66dddb7258a1108198f9e9719515da
Glitch (glitch-hello-node) <none>
2024-12-19 14:57:53 +0000

    🏃🍟 Checkpoint ./public/script.js:70058483/2137 ./public/index.html:70058481/404:70058483/54 ./public/style.css:70058481/2

--- public/script.js
+++ public/script.js
@@ -1,37 +1,20 @@
-class Picker {
-  constructor(target, width, height) {
-    this.target = target;
-    this.width = width;
-    this.height = height;
-    this.target.width = width;
-    this.target.height = height;
-    this.target.getContext("2d");
-    this.pickerCircle = { x: 10, y: 10, width: 7, height: 7};
-  }
-}
-
-draw() {
-  
-}
-
-build() {
-  let gradient = this.conext.createLinearGradient(0, 0,
-this.width, 0);
-  
-  gradient.addColorStop(0, "rgb(255, 0, 0)");
-  gradient.addColorStop(0.15, "rgb(255, 0, 255)");
-  gradient.addColorStop(0.33, "rgb(0, 0, 255)");
-  gradient.addColorStop(0.49, "rgb(0, 255, 255)");
-  gradient.addColorStop(0.67, "rgb(0, 255, 0)");
-  gradient.addColorStop(0.84, "rgb(255, 255, 0)");
-  gradient.addColorStop(1, "rgb(255, 0, 0)");
-  
-  this.context.fillStyle = gradient;
-  this.context.fillRect(0, 0, this.width, this.height)
-
-  }
+$(document).ready(function(){
+  $("a").on('click', function(event) {
 
 
+    if (this.hash !== "") {
 
-}
+      event.preventDefault();
 
+
+      var hash = this.hash;
+
+
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+        window.location.hash = hash;
+      });
+    } 
+  });
+});
