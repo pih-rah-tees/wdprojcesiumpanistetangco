9c73419dba3e7966e61c2ef4e229bc1337db5a3c
Glitch (glitch-hello-node) <none>
2024-12-19 14:35:17 +0000

    🛀🕶 Checkpoint ./public/script.js:70058483/198 ./public/Draw.html:70058483/23 ./.glitch-assets:70058481/967 ./public/index.html:70058481/588:70058483/127 ./public/style.css:70058481/21:70058483/30

--- public/script.js
+++ public/script.js
@@ -20,11 +20,18 @@ this.width, 0);
   
   gradient.addColorStop(0, "rgb(255, 0, 0)");
   gradient.addColorStop(0.15, "rgb(255, 0, 255)");
-  gradient.addColorStop(0.33, "rgb(255, 0, 0)");
-  gradient.addColorStop(049, "rgb(255, 0, 0)");
-  gradient.addColorStop(0, "rgb(255, 0, 0)");
-  gradient.addColorStop(0, "rgb(255, 0, 0)");
-  gradient.addColorStop(0, "rgb(255, 0, 0)");
-  gradient.addColorStop(0, "rgb(255, 0, 0)");
+  gradient.addColorStop(0.33, "rgb(0, 0, 255)");
+  gradient.addColorStop(0.49, "rgb(0, 255, 255)");
+  gradient.addColorStop(0.67, "rgb(0, 255, 0)");
+  gradient.addColorStop(0.84, "rgb(255, 255, 0)");
+  gradient.addColorStop(1, "rgb(255, 0, 0)");
+  
+  this.context.fillStyle = gradient;
+  this.context.fillRect(0, 0, this.width, this.height)
+
+  }
+
+
+
 }
 
