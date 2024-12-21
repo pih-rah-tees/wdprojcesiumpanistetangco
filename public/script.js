43f6c44cdded1ddc1d95f169e35834180cf7d298
Glitch (wdprojcesiumpanistetangco) <none>
2024-12-21 17:04:42 +0000

    🚊❣️ Checkpoint ./public/design.css:70058483/187 ./public/script.js:70058481/3438:70058483/1 ./.glitch-assets:70058481/43 ./public/index.html:70058481/216:70058483/1296

--- public/script.js
+++ public/script.js
@@ -1,38 +1,15 @@
-//does not work
-$(document).ready(function () {
-  $("a").on("click", function (event) {
-    if (this.hash !== "") {
-      event.preventDefault();
-
-      var hash = this.hash;
-
-      $("html, body").animate(
-        {
-          scrollTop: $(hash).offset().top,
-        },
-        800,
-        function () {
-          window.location.hash = hash;
-        }
-      );
-    }
-  });
-});
-
+//brush for draw canvas *not working
 window.addEventListener("load", () => {
   canvas.width = canvas.offsetWidth;
   canvas.height = canvas.offsetHeight;
 });
-
 const canvas = document.querySelector("canvas"),
   ctx = canvas.getContext("2d");
-
 const drawing = (e) => {
   ctx.lineto(e.offsetX, e.offsetY);
   ctx.stroke();
 };
 canvas.addEventListener("mousemove", drawing);
-
 
 //works and is for the design button color change
 function buttonFunc(a) {
