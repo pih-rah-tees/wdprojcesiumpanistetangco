a88ef1020f3865abb966e51fdad4ea801b0904c4
Glitch (wdprojcesiumpanistetangco) <none>
2024-12-21 16:04:46 +0000

    🎙⛷ Checkpoint ./public/design.css:70058481/67:70058483/61 ./public/Design.html:70058483/42 ./public/style.css:70058481/13055 ./public/draw.css:70058481/99:70058483/5 ./public/index.html:70058481/196 ./public/script.js:70058481/3429

--- public/script.js
+++ public/script.js
@@ -1,55 +1,68 @@
-$(document).ready(function(){
-  $("a").on('click', function(event) {
-
-
+$(document).ready(function () {
+  $("a").on("click", function (event) {
     if (this.hash !== "") {
-
       event.preventDefault();
-
 
       var hash = this.hash;
 
-
-      $('html, body').animate({
-        scrollTop: $(hash).offset().top
-      }, 800, function(){
-        window.location.hash = hash;
-      });
-    } 
+      $("html, body").animate(
+        {
+          scrollTop: $(hash).offset().top,
+        },
+        800,
+        function () {
+          window.location.hash = hash;
+        }
+      );
+    }
   });
 });
 
-
 window.addEventListener("load", () => {
   canvas.width = canvas.offsetWidth;
-  canvas.height = canvas.offsetHeight
-}) 
-                        
+  canvas.height = canvas.offsetHeight;
+});
 
 const canvas = document.querySelector("canvas"),
-ctx = canvas.getContext("2d");
-
+  ctx = canvas.getContext("2d");
 
 const drawing = (e) => {
   ctx.lineto(e.offsetX, e.offsetY);
   ctx.stroke();
-}
+};
 canvas.addEventListener("mousemove", drawing);
 
-
 function buttonFunc(a) {
-  var id = "pattern"+a;
+  var id = "pattern" + a;
   if (id == "pattern1") {
-    document.getElementById("pattern1").setAttribute('style', 'background-color: #ad88c6;');
-    document.getElementById("pattern2").setAttribute('style', 'background-color: #E1AFD1;');
-    document.getElementById("pattern3").setAttribute('style', 'background-color: #E1AFD1;');
+    document
+      .getElementById("pattern1")
+      .setAttribute("style", "background-color: #ad88c6;");
+    document
+      .getElementById("pattern2")
+      .setAttribute("style", "background-color: #E1AFD1;");
+    document
+      .getElementById("pattern3")
+      .setAttribute("style", "background-color: #E1AFD1;");
   } else if (id == "pattern2") {
-    document.getElementById("pattern1").setAttribute('style', 'background-color: #E1AFD1;');
-    document.getElementById("pattern2").setAttribute('style', 'background-color: #ad88c6;');
-    document.getElementById("pattern3").setAttribute('style', 'background-color: #E1AFD1;');
+    document
+      .getElementById("pattern1")
+      .setAttribute("style", "background-color: #E1AFD1;");
+    document
+      .getElementById("pattern2")
+      .setAttribute("style", "background-color: #ad88c6;");
+    document
+      .getElementById("pattern3")
+      .setAttribute("style", "background-color: #E1AFD1;");
   } else {
-    document.getElementById("pattern1").setAttribute('style', 'background-color: #E1AFD1;');
-    document.getElementById("pattern2").setAttribute('style', 'background-color: #E1AFD1;');
-    document.getElementById("pattern3").setAttribute('style', 'background-color: #ad88c6;');
+    document
+      .getElementById("pattern1")
+      .setAttribute("style", "background-color: #E1AFD1;");
+    document
+      .getElementById("pattern2")
+      .setAttribute("style", "background-color: #E1AFD1;");
+    document
+      .getElementById("pattern3")
+      .setAttribute("style", "background-color: #ad88c6;");
   }
 }
