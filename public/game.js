9af19623fd25ad6829787011b19765271f261e62
Glitch (wdprojcesiumpanistetangco) <none>
2025-02-03 11:32:42 +0000

    🌸🍎 Checkpoint ./public/Game.html:70058483/1186 ./public/game.css:70058483/144 ./public/game.js:70058483/473

---
+++ public/game.js
@@ -0,0 +1,20 @@
+"use strict";
+
+var cn = document.getElementById("canvas");
+var cx = cn.getContext("2d");
+
+//images
+var butterfly = new Image();
+var bg = new Image();
+var fg = new Image();
+var pillarUp = new Image();
+var pillarDown = new Image();
+
+butterfly.src = "https://cdn.glitch.global/fdf6058e-73c2-42ca-8d01-e253e1a6e82b/BUTTERFLY.png?v=1734622012979"
+bg.src = ""
+fg.src = ""
+pillarUp.src = ""
+pillarDown.src = ""
+
+//variables
+var gap = 
