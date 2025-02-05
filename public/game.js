86e14c0772354bfd503d4809ff02074a0390b419
Glitch (wdprojcesiumpanistetangco) <none>
2025-02-05 08:25:52 +0000

    💘🍐 Checkpoint ./public/game.js:70058483/493 ./public/Game.html:70058483/29

--- public/game.js
+++ public/game.js
@@ -1,3 +1,8 @@
-myScore() {
-  if document.getElementById("butterfly")
+var score = document.getElementById("score")
+
+function myScore() {
+  if (document.getElementById("butterfly")
 }
+  
+document.write("Score")
+
