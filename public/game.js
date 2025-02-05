b2192a3d1da52c84bc61db9c2e6a6e9c7c2e53db
Glitch (wdprojcesiumpanistetangco) <none>
2025-02-05 09:23:07 +0000

    💴👦 Checkpoint ./public/Game.html:70058483/634 ./public/game.js:70058483/538 ./public/game.css:70058483/183

--- public/game.js
+++
@@ -1,5 +0,0 @@
-function myScore(click){
-const sum = document.getElementById('scoreCount')
-const sumvalue = parseInt(document.getElementById('scoreCount')) + click;
-document.getElementById('scoreCount') = sumvalue;  
-}
